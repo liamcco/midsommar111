ec7c552d9d336d4cc9372caa3c2cdfc749d7c3fc
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2024-04-22 14:03:44 +0200

    refactor

--- src/app/hosts/page.tsx
+++ src/app/hosts/page.tsx
@@ -2,39 +2,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-12 pt-24 lg:p-0">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center backdrop-blur-2xl dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:p-4">
-          <a href="/">
-            <Image
-              src="/midsommarfest.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={350}
-              height={24}
-              priority
-            />
-          </a>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="/"
-            rel="noopener noreferrer"
-          >
-            Midsommar i{" "}
-            <Image
-              src="/gunrabol.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={150}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
+    <>
       <div
         className="mb-64 mt-8 text-center lg:mb-0 lg:w-full lg:max-w-5xl"
         id="dante"
@@ -61,6 +29,6 @@ export default function Page() {
       </div>
 
       <div></div>
-    </main>
+    </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -22,7 +23,42 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"
         ></link>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col items-center justify-between px-12 pt-24 lg:p-0">
+          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+            <a href="/">
+              <p className="fixed left-0 top-0 flex w-full justify-center backdrop-blur-2xl blurred dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:p-4">
+                <Image
+                  src="/midsommarfest.svg"
+                  alt="midsommarfest"
+                  className="dark:invert"
+                  width={350}
+                  height={150}
+                  priority
+                />
+              </p>
+            </a>
+            <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+              <a
+                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                href="/"
+                rel="noopener noreferrer"
+              >
+                Midsommar i{" "}
+                <Image
+                  src="/gunrabol.svg"
+                  alt="Vercel Logo"
+                  className="dark:invert"
+                  width={150}
+                  height={24}
+                  priority
+                />
+              </a>
+            </div>
+          </div>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
