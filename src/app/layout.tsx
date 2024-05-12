f07708a2760efb056685b758587b77b06ab35cb0
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2024-05-12 21:26:20 +0200

    update SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Midsommar i Gundrabol",
-  description: "Det största midsommarfirandet i Gundrabol!",
+  title: "Midsommar på Edsgatan",
+  description: "Fira med oss!",
 };
 
 export default function RootLayout({
